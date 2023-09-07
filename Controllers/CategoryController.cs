eb2e9e658137b8549069d906e73455e4a23267ee
John-Major <johnpmajor96@gmail.com>
2023-09-06 21:17:56 -0400

    added display table data

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,7 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 
 public class CategoryController : Controller {
+    private readonly ApplicationDbContext db;
+    public CategoryController(ApplicationDbContext db){
+        this.db = db;
+    }
     public IActionResult Index(){
-        return View();
+        IEnumerable<Category> objCategoryList = db.Categories;
+        return View(objCategoryList);
     }
 }
