a5d5e0cdc6b244103c85a580d904bd301e3ac453
John-Major <johnpmajor96@gmail.com>
2023-09-06 20:37:32 -0400

    added Category model

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Category{
+    [Key]
+    public int ID {get; set;}
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    
+    //Default DateTime property
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now; 
+}
